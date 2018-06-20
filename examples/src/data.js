06ac19d55d3ab6edd7c150459be04a95be53b35f
Gionatan Lombardi <gionatan@vangogh-creative.it>
2018-06-20 10:15:44 +0200

    CustomSelect - optionsTree render in <select>

--- examples/src/data.js
+++ examples/src/data.js
@@ -1,5 +1,9 @@
 export default [
   {
+    label: 'Select...',
+    value: '',
+  },
+  {
     label: 'Foo',
     value: 'foo',
   },
@@ -11,26 +15,17 @@ export default [
     label: 'My Option Group',
     children: [
       {
-        label: 'Long but very long option label',
+        label: 'Long, but very long option label',
         value: 'long-option-label'
-      },
-      {
-        label: 'Nested Option Group with long label',
-        children: [
-          {
-            label: 'Cat',
-            value: 'cat',
-          },
-          {
-            label: 'Tiger',
-            value: 'tiger',
-          }
-        ],
       },
       {
         label: 'Short',
         value: 'short-option-label'
       },
     ],
+  },
+  {
+    label: 'Fuzzy',
+    value: 'fuzzy',
   },
 ]
--- src/index.js
+++ src/index.js
@@ -20,9 +20,25 @@ class CustomSelect extends Component{
 
     return (
       <select {...optionalProps}>
+        {optionsTree.map((item) => this._RenderTree(item))}
       </select>
     );
   }
+
+  _RenderTree = (item) => {
+    if (item.children) {
+      return this._RenderOptgroup(item)
+    }
+    return this._RenderOption(item)
+  }
+
+  _RenderOption = (item) => <option key={`${item.label}-${item.value}`} value={item.value}>{item.label}</option>
+
+  _RenderOptgroup = (item) => (
+    <optgroup key={item.label} label={item.label}>
+      { item.children && item.children.filter(option => option.label && option.value).map(option => this._RenderOption(option))}
+    </optgroup>
+  )
 }
 
 CustomSelect.propTypes = {
